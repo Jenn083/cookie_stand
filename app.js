766fce22320c89de3140b93cdf2bed85b5d628dc
jenn083 <nguyenj1203@gmail.com>
2016-12-06 01:31:04 -0800

    added capital hill object

--- app.js
+++ app.js
@@ -147,3 +147,52 @@ var seattleCenter = {
 seattleCenter.render();
 seattleCenter.randCustPerHour();
 seattleCenter.cookiesSoldPerHour;
+
+var capitalHill = {
+
+  minCustPerHour: 11,
+  maxCustPerHour: 38,
+  avgCookiesPerHour: 3.7,
+  cookiesSoldPerHour: [],
+
+  randCustPerHour: function () {
+    console.log('before running loop in randCustPerHour method: ' + this.cookiesSoldPerHour);
+    for(var i = 0; i< hours.length;i++){
+      var randomCustomersPerHour =  Math.ceil(((Math.random()*this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+      var cookiesPerHour = Math.ceil(randomCustomersPerHour * this.avgCookiesPerHour);
+      this.cookiesSoldPerHour.push(cookiesPerHour);
+      // console.log(this.cookiesSoldPerHour);
+      //Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    console.log('after running loop in randCustPerHour method: ' + this.cookiesSoldPerHour);
+  },
+  sumTotal: function() {
+    var sum = 0;
+    for(var i = 0; i < this.cookiesSoldPerHour.length;i++) {
+      sum += this.cookiesSoldPerHour[i];
+    }
+    return sum;
+  },
+  render: function() {
+    this.randCustPerHour();// before you render so that there's content to render
+    var ulEl = document.createElement('ul');
+    ulEl.textContent = 'Captial Hill';
+    document.body.appendChild(ulEl);
+
+    for(var i = 0; i< hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ':  ' + this.cookiesSoldPerHour[i];
+      ulEl.appendChild(liEl);
+
+    }
+    var pEl =document.createElement('p');
+    pEl.textContent = 'Total:   ' + this.sumTotal();
+    document.body.appendChild(pEl);
+  }
+
+}
+
+
+capitalHill.render();
+capitalHill.randCustPerHour();
+capitalHill.cookiesSoldPerHour;
